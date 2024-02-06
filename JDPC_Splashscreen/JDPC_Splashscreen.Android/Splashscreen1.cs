cece3090809042e4ba6f9b774959748e8dab6b9a
LDVP5049 <luismaster752@gmail.com>
2024-02-06 08:10:43 -0700

    Agregar archivos de proyecto.

---
+++ JDPC_Splashscreen/JDPC_Splashscreen.Android/Splashscreen1.cs
@@ -0,0 +1,36 @@
+﻿using Android.App;
+using Android.Content;
+using Android.Content.PM;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace JDPC_Splashscreen.Droid
+{
+    [Activity(Label = "JDPC_Splashscreen", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize )]
+    //    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+
+    public class Splashscreen1 : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            //para suscribirnos a MainActivity
+            StartActivity(new Intent(Application.Context, typeof(MainActivity)));
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
+            LoadApplication(new App());
+        }
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+}
